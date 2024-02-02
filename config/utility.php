81891bb13854c828ffb660d881edf4ee4a539b0f
bedrimisini1 <158111329+bedrimisini1@users.noreply.github.com>
2024-02-02 23:49:56 +0100

    Update utility.php

--- config/utility.php
+++ config/utility.php
@@ -1,46 +1,71 @@
 <?php
 
-class utility{
-    public static function setmessage($type,$message,$lokacioni){
+class utility
+{
+    public static function setmessage($type, $message, $lokacioni)
+    {
         $_SESSION['type'] = $type;
         $_SESSION['message'] = $message;
         header("Location: $lokacioni");
     }
 
-    public function showmessage(){
+    public function showmessage()
+    {
         if (isset($_SESSION['type']) && isset($_SESSION['message'])) {
             $msg = $_SESSION["message"];
-            if($_SESSION['type'] === 'success')
-            {
-            echo "<body onload=\"showSuccessToast('$msg')\">";
-            } else if($_SESSION["type"] === 'error')
-            {
+            if ($_SESSION['type'] === 'success') {
+                echo "<body onload=\"showSuccessToast('$msg')\">";
+            } else if ($_SESSION["type"] === 'error') {
                 echo "<body onload=\"showErrorToast('$msg')\">";
             }
             unset($_SESSION['type']);
             unset($_SESSION['message']);
         }
-
     }
-    public function redirecthome(){
-        if(!(empty($_SESSION['user_id']))){
+    public function redirecthome()
+    {
+        if (!(empty($_SESSION['user_id']))) {
             header("Location: /manage/index.php");
             exit();
         }
     }
-    public function redirectlogin(){
-        if(empty($_SESSION['user_id'])){
+    public function redirectlogin()
+    {
+        if (empty($_SESSION['user_id'])) {
             header("Location: /login.php");
             exit();
         }
     }
-    public function headerbuttonat(){
-        if($_SESSION['roli'] === 'admin'){
-            echo '<a class="link-head" href="/home">Home</a>';
-            echo '<a class="link-head" href="/userlist">Lista e Klienteve</a>'; 
-            echo '<a class="link-head" href="/postlist">Lista e postev</a>';
+    public function headerbuttonat()
+    {
+        if ($_SESSION['roli'] === 'admin') {
+            echo '<a class="link-head" href="/manage/index.php">Home</a>';
+            echo '<a class="link-head" href="/manage/userlist.php?type=user">Lista e Klienteve</a>';
+            echo '<a class="link-head" href="/manage/userlist.php?type=admin">Lista e Adminave</a>';
+        } else {
+            echo '<a class="link-head" href="/manage/index.php">Home</a>';
+        }
+    }
+    public function onlyAdminAccess()
+    {
+        if ($_SESSION['roli'] != 'admin') {
+            $this->setmessage('error', "Nuk keni akses ne kete faqe", '/manage/index.php');
+        }
+    }
+    public function userlist()
+    {
+        if (isset($_GET['type'])) {
+            if ($_GET['type'] == 'admin') {
+                include 'userlist/adminlist.php';
+            } elseif ($_GET['type'] == 'user') {
+                include 'userlist/userlist.php';
+            } else {
+                $this->setmessage('error', "Nuk u shfaq asgje", '/manage/index.php');
+            }
+        } else if (isset($_GET['shtouser'])) {
+            include 'userlist/shtouser.php';
         }else{
-            echo '<a class="link-head" href="/home">Home</a>';
+            $this->setmessage('error', "Nuk u shfaq asgje", '/manage/index.php');
         }
     }
 }
